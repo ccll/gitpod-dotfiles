367795efce0f292633520e02923a52b7837bff64
Cai Lei <cailei@live.com>
2022-05-28 04:11:39 +0000

    Install bash resources.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Install bash resources.
+mkdir -p ~/.bashrc.d
+cp 10-alias.bashrc ~/bashrc.d/
+
+# Install fish resources.
 mkdir -p ~/.config/fish/conf.d
 cp 10-show-full-pwd-path.fish ~/.config/fish/conf.d/
 cp 20-alias.fish ~/.config/fish/conf.d/
